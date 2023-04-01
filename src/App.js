6b669f7984fe491621de87c73ca43ed6fe3ce414
shoaib-ahammde <ahammed15-4955@diu.edu.bd>
2023-04-01 15:02:18 +0600

    create button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useSyncExternalStore } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Counter ></Counter>
     </div>
+
   );
 }
+ function Counter() {
+    const [count,setCount]= useState(44);
+
+    const increseCount=()=>{
+      setCount(count+1);
+    }
+    const decreaseCount=()=>{
+      setCount(count - 1);
+    }
+   
+      return(
+        <div>
+          <h1>count:{count}</h1>
+          <button onClick={increseCount}>Increase</button>
+          <button onClick={decreaseCount}>Decrease</button>
+        </div>
+      )
+    }
 
 export default App;
